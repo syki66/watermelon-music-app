aacc01416dbcb3c95e10b16e3ce91ec7f6dc1a53
syki66 <66syki@gmail.com>
2020-01-22 00:55:09 +0900

    ha.....

--- App.js
+++ App.js
@@ -1,76 +1,117 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text, Alert, View, SafeAreaView, ScrollView } from 'react-native';
 import cheerio from 'cheerio-without-node-native';
 import axios from 'axios';
 import Chart from './Chart';
 import { WebView } from 'react-native-webview';
 
 
-
 const MELON_LINK = "https://www.melon.com/chart/index.htm#params%5Bidx%5D=1";
 
 
 
-
-export default class extends React.Component {
+export default class App extends React.Component {
   state = {
     isLoading: true,
-    chartData: []
+    rankData: []
   }
-  
 
 
   getChartData = async() => {
-
-    let chartArray = [];
+    let rankArray = [];
 
     const res = await axios.get(MELON_LINK);
     const $ = cheerio.load(res.data);
 
     // 랭킹 가져오기
     $('.rank').each( (index, element) => {
-      chartArray[index] = {
+      rankArray[index] = {
         rank : $(element).text()
       }
     });
 
     // 제목 가져오기
     $('.rank01').each( (index, element) => {
-      chartArray[index+1].title = $(element).text().trim();
+      rankArray[index+1].title = $(element).text().trim();
     });
 
     // 가수 이름 가져오기
     $('.rank02').each( (index, element) => {
       let eachLength = $(element).text().trim().length;
-      chartArray[index+1].name = $(element).text().trim().substr(0,eachLength/2);
+      rankArray[index+1].name = $(element).text().trim().substr(0,eachLength/2);
     });
 
     // 앨범 커버 가져오기
     $('.image_typeAll').each( (index, element) => {
-      chartArray[index+1].cover = $(element).find('img').attr('src');
+      rankArray[index+1].cover = $(element).find('img').attr('src');
     });
 
-    chartArray.splice(0,1);
-    this.setState({isLoading: false, chartData: chartArray});
-    //console.log(chartArray);
+    rankArray.splice(0,1); //첫번째 쓰레기값 제거
+
+
+    this.setState({rankData: rankArray});
+
+    //this.setState({rankData: rankArray});
+
+
+    
+    // html 형식 iframe 값 가져오기
+    // rankArray.map((each, i) => {
+    //   rankArray[i].iframe = this.getSearchData(each.title, each.name);
+    // })
+    
+
+    // rankArray.map((each, i) => {
+    //   this.getSearchData(each, i, each.title, each.name);
+    // })
+    
+    
+    // for (let i = 0; i < rankArray.length; i++) {
+    //   rankArray[i].iframe =  this.getSearchData(rankArray[i].title, rankArray[i].name);
+    // } 
+
+
+
+    
+
+
+    // rankArray.map( (each) => {
+    //   each.iframe = this.getSearchData(each.title, each.name);
+      
+    // })
+  
+
   }
 
 
-  getSearchData = async() => {
+
+  // getSearchData = async(title, name) => {
+
+  //   const searchUrl = `https://api.soundcloud.com/tracks?q=${title}%20${name}&format=json&client_id=MhsRoDc6eXwJmBNd2ph1Lih2atDZEiG3`;
+  //   console.log("1번", searchUrl);
+  //   const searchData = await axios.get(searchUrl);
+    
+  //   const soundCloudUrl = searchData.data[0].permalink_url
+  //   console.log("2번", soundCloudUrl);
+
+  //   const iframeUrl = `https://soundcloud.com/oembed.json?auto_play=true&url=${soundCloudUrl}`;
+  //   const iframeData = await axios.get(iframeUrl);
+  //   const iframeHtml = iframeData.data.html;
+  //   console.log("3번", iframeHtml);
+  // }
 
   
-    let title = '방탄소년단 black swan';
-    let search_url = `https://api.soundcloud.com/tracks?q=${title}&format=json&client_id=MhsRoDc6eXwJmBNd2ph1Lih2atDZEiG3`;
-  
-    const data = await axios.get(search_url);
-    
-    
-    console.log(data.data[0].permalink_url);
-  }
+  componentDidMount = async() => {
+    await this.getChartData();
 
-  componentDidMount() {
-    this.getChartData();
-    this.getSearchData();
+    // let searchArray = [];
+
+    // this.state.rankData.map( (each, i) => {
+    //   searchArray[i] = getSearchData(each.name, each.title);
+    // })
+
+    console.log(this.state.rankData);
+    this.setState({isLoading: false});
   }
 
   render() {
@@ -79,14 +120,15 @@ export default class extends React.Component {
         <Text>아직로딩중</Text>
       ) : (
           <SafeAreaView>
+            <View style={styles.container}>
+              <WebView
+                source={{ html: '<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=true&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F525830577&show_artwork=true&auto_play=true"></iframe>' }}
+              />
+            </View>
             <ScrollView>
-              <View style={{height: 400}}>
-            <WebView
-                    source={{ html: '<iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?visual=false&url=https%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F324224226&sharing=false&show_user=false&"></iframe>' }}
-                />
-                </View>
+
               <View>
-                {this.state.chartData.map((each) => {
+                {this.state.rankData.map((each) => {
                   return <Chart
                     key={each.rank}
                     rank={each.rank}
@@ -103,3 +145,9 @@ export default class extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    position: "relative"
+  }
+});
--- Chart.js
+++ Chart.js
@@ -1,39 +1,76 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, StatusBar, SectionList, SafeAreaView, ScrollView } from 'react-native';
+import { StyleSheet, Text, Alert, TouchableOpacity, Button, View, Image, StatusBar, SectionList, SafeAreaView, ScrollView } from 'react-native';
 import { WebView } from 'react-native-webview';
+import axios from 'axios';
+
+
+// getSearchData = async(title, name) => {
+
+//     const searchUrl = `https://api.soundcloud.com/tracks?q=${title}%20${name}&format=json&client_id=MhsRoDc6eXwJmBNd2ph1Lih2atDZEiG3`;
+  
+//     const searchData = await axios.get(searchUrl);
+//     const soundCloudUrl = searchData.data[0].permalink_url
+
+//     const iframeUrl = `https://soundcloud.com/oembed.json?auto_play=true&url=${soundCloudUrl}`;
+//     const iframeData = await axios.get(iframeUrl);
+//     const iframeHtml = iframeData.data.html;
+//     console.log(iframeHtml);
+//     console.log(App.App.state.isLoading);
+//   }
 
 
 
 export default function Chart({rank, title, name, cover}) {
 
     return (
-        <View style={styles.container}>
-            <View style={styles.first}>
-                <Image
-                    style={{ width: 50, height: 50 }}
-                    source={{ uri: cover }}
-                />
-                <Text>{rank}위 - {title} - {name}</Text>
-            </View>
+        <TouchableOpacity
+            onPress={() => { alert(title, name) }}
+            style={styles.container}>
 
-            <View style={styles.second}>
+            <Image
+                style={{ width: 70, height: 70 }}
+                source={{ uri: cover }}
+            />
+
+            <View style={styles.song_info}>
+                <Text style={styles.song_info_rank}>{rank}</Text>
+                <View style={styles.song_info_2}>
+                    <Text style={styles.song_info_2_title}>{title}</Text>
+                    <Text style={styles.song_info_2_name}>{name}</Text>
+                </View>
 
             </View>
-        </View>
+        </TouchableOpacity>
+
+
 
     );
 }
 
-const styles =StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'row'
     },
-    first: {
+    song_info: {
+        flex: 1,
+        flexDirection: 'row',
+        borderTopWidth: 0.5,
+        borderColor: '#d6d7da',
+        alignItems: 'center'
+    },
+    song_info_rank: {
+        fontSize:50,
+        width: 55
+    },
+    song_info_2: {
         flex: 1
     },
-    second: {
+    song_info_2_title: {
         flex: 1,
-        backgroundColor: 'red'
+        fontSize: 25
+    },
+    song_info_2_name: {
+        flex: 1
     }
 })
